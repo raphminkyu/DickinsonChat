6e9e9b3a6429d830b6494f87d3eaabbcb98fd00e
raphminkyu <leer@dickinson.edu>
2019-09-29 19:03:25 -0400

    Before CSS works

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Message(models.Model):
+    #if the user is deleted, all the messaged related to the user will be deleted
+    author = models.ForeignKey(User, related_name = "author_messages", on_delete = models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add = True)
+
+
+    def __str__(self):
+        return self.author.username
+    
+    #load only the last 10 messages
+    def last_10_messages(self):
+        return Message.objects.order_by("-timestamp").all()[:10]
